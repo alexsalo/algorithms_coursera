2b9aa72f22466b6ff1e7e46abe9b0389e15239e9
alexsalo <alexsalovrn@gmail.com>
2015-11-09 09:07:29 -0600

    parethesis valid combi DP

--- src/dynamic_programming/ValidCombinations.java
+++ src/dynamic_programming/ValidCombinations.java
@@ -1,5 +1,8 @@
 package dynamic_programming;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Print all valid combinations of ( ) 
  * ex:
@@ -25,14 +28,23 @@ package dynamic_programming;
  */
 public class ValidCombinations {
     
-    /*public static String printParens(int N) {
-
-        int nl = 0;
-        while (nl < N);
-    }*/
+    public static Queue<String> genParethesis(String prefix, int N, Queue<String> q, int cnt) {
+        if (prefix.length() >= 2 * N) {
+            if (cnt == 0)
+                q.add(prefix);
+            return q;
+        } 
+        
+        if (cnt > 0)
+            q = genParethesis(prefix + ')', N, q, cnt - 1);
+        q = genParethesis(prefix + '(', N, q, cnt + 1);
+        
+        return q;
+    }
+    
 
     public static void main(String[] args) {
-        //System.out.println(printParens(3));
+        System.out.println(genParethesis("", 4, new LinkedList<String>(), 0));
     }
 
 }
